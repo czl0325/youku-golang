c1a879ab44471e065a684579898bea490c82acde
czl0325 <>
2021-04-22 14:44:03 +0800

    新建后端工程

---
+++ youku-backend/youku/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "youku/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		orm.Debug = true
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
